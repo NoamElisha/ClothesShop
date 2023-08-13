2ae53146e1bf32bfc28e0491ba66a440c80cfbed
NoamElisha <noampro21@gmail.com>
2023-08-13 23:08:58 +0300

    (feat) delete irrelevant files

--- test/src/Helpers/getClientsUsernames.js
+++
@@ -1,9 +0,0 @@
-import Axios from "../API/axios.js";
-
-async function getClientsUsername(){
-    const response = await Axios.get("/clients/usernames");
-    const { data } = response;
-    return data;
-}
-// when i see pending use awiat
-//console.log(await getClientsUsername());
--- test/src/Helpers/getGarments.js
+++
@@ -1,9 +0,0 @@
-import Axios from "../API/axios.js";
-
-async function getGarments(){
-    const response = await Axios.get("/garments");
-    const { data } = response;
-    return data;
-}
-// when i see pending use awiat
-//console.log(await getGarments());
